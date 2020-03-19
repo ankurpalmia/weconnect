629108a970032f78b020fa3cf9457031b7e269b9
Ankur Palmia <ankur.palmia@joshtechnologygroup.com>
2020-03-19 19:41:46 +0530

    wc-9: removed unecessary function from utils.py

--- post/utils.py
+++ post/utils.py
@@ -10,21 +10,4 @@ def get_friends(user):
         else:
             friends_list.append(friend.sender)
     return friends_list
-
-
-def create_post_dict(all_posts):
-    post_list = []
-    for post in all_posts:
-        post_list.append({
-            'text': post.text,
-            'image': post.image if post.image else None,
-            'created_by': {
-                'name': post.created_by.get_full_name(),
-                'username': post.created_by.username
-            },
-            'likes': post.liked_by.count(),
-            'liked_by': [person.get_full_name() for person in post.liked_by.all()],
-            'created_at': post.created_at
-        })
-    return post_list
         
